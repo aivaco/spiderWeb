6c6754af4a25439ab18d455e079259387215ab42
aivaco <aivaco@outlook.com>
2017-05-14 21:13:44 -0600

    Cambiado para que ejecute con parámetros.

--- src/com/spider/Main.java
+++ src/com/spider/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
         WebCrawler webCrawler = new WebCrawler();
         webCrawler.loadSeeds();
-        //webCrawler.startThreads(Integer.parseInt(args[0]),Integer.parseInt(args[1]),Integer.parseInt(args[2]));
-        webCrawler.startThreads(300,500, 5,false);
+        webCrawler.startThreads(Integer.parseInt(args[0]),Integer.parseInt(args[1]),Integer.parseInt(args[2]), Boolean.valueOf(args[3]));
+        //webCrawler.startThreads(300,500, 5,false);
 
     }
 }
